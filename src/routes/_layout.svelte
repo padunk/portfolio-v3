ea658e884bbc26658f4d0f00de2d567d303b5197
Padunk <abraham.agel@gmail.com>
2020-11-25 11:09:54 +0700

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Nav from '../components/Nav.svelte';
+
+	export let segment: string;
+</script>
+
+<style>
+	main {
+		position: relative;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
