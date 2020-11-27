e36f2845096e7ce853aa733f6dfd0600482fb96e
Padunk <abraham.agel@gmail.com>
2020-11-27 15:11:46 +0700

    chevron svg

---
+++ src/components/ChevronUp.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let size: number;
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="{size}"
+    height="{size}"
+    viewBox="0 0 24 24"
+>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
+</svg>
