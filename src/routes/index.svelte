c1a9083d7adf0ae29c002288779c927a3b196d35
Padunk <abraham.agel@gmail.com>
2020-11-28 10:04:01 +0700

    change styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,21 @@
 <script>
-	import MainNav from '../components/MainNav.svelte'
+    import MainNav from "../components/MainNav.svelte";
 </script>
 
 <svelte:head>
-	<title>Abraham Anak Agung</title>
+    <title>Abraham Anak Agung</title>
 </svelte:head>
 
 <h1 hidden>Abraham Anak Agung</h1>
 
-<section id='main'>
-	<MainNav />
+<section id="main">
+    <MainNav />
 </section>
 
 <style>
-	#main {
-		display: grid;
-    	place-items: center;
-    	min-height: 100vh;
-	}
+    #main {
+        /* display: grid;
+    	place-items: center; */
+        min-height: 100vh;
+    }
 </style>
