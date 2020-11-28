91e626e5e1808367d23494c2db71da35d3e4104d
Padunk <abraham.agel@gmail.com>
2020-11-28 10:04:14 +0700

    utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const COLORS = {
+    primary: "hsl(260, 47%, 27%)",
+    primaryLight: "hsl(265, 21%, 53%)",
+    secondary: "hsl(328,53%,69%)",
+    grayish: "hsl(247,29%,66%)",
+};
---
+++ src/utils/helpers.ts
@@ -0,0 +1,1 @@
+export const isMobile = (): boolean => window && window.innerWidth < 720;
