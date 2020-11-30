80e86db0eef9f40063fa905944122afb7a93cab8
Padunk <abraham.agel@gmail.com>
2020-11-30 12:10:51 +0700

    create skills stack

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -88,6 +88,25 @@
             );
         }, 2000);
     };
+
+    const stacks = [
+        { name: "ReactJS", url: "https://reactjs.org/" },
+        { name: "ReactNative", url: "https://reactnative.dev/" },
+        { name: "Redux", url: "https://redux.js.org/" },
+        { name: "TypeScript", url: "https://www.typescriptlang.org/" },
+        { name: "Firebase", url: "https://firebase.google.com" },
+        { name: "TailwindCSS", url: "https://tailwindcss.com" },
+        { name: "Styled Components", url: "https://styled-components.com" },
+        { name: "NodeJS", url: "https://nodejs.org/en/" },
+    ];
+
+    const otherStacks = [
+        { name: "Svelte", url: "https://svelte.dev" },
+        { name: "PostgreSQL", url: "https://www.postgresql.org/" },
+        { name: "MongoDB", url: "https://mongodb.com" },
+        { name: "SASS", url: "https://sass-lang.com/" },
+        { name: "Go", url: "https://golang.org" },
+    ];
 </script>
 
 <svelte:head>
@@ -103,9 +122,41 @@
         <div class="about-me__content">
             <span class="waving-hand" on:click="{virtualShakeHand}">👋</span>
             <p>I'm a web developer based in Indonesia (GMT +7).</p>
+            <p>My current tools is:</p>
+            <ul>
+                {#each stacks as stack}
+                    <li>
+                        <a
+                            class="stack-link"
+                            href="{stack.url}"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >{stack.name}</a>
+                    </li>
+                {/each}
+            </ul>
+            <p>Beside that I'm also work with other tech such as:</p>
+            <ul>
+                {#each otherStacks as otherStack}
+                    <li>
+                        <a
+                            class="stack-link"
+                            href="{otherStack.url}"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >{otherStack.name}</a>
+                    </li>
+                {/each}
+            </ul>
             <p>
-                I learn how to make a website since 2017, and have been fall in
-                love since then.
+                Currently I'm expanding my skill with
+                <a
+                    href="https://aws.amazon.com/id/amplify/"
+                    class="link-gradient"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >AWS - Amplify</a>
+                to get better understanding about serverless.
             </p>
         </div>
     </article>
@@ -113,7 +164,7 @@
         If you on Twitter, say
         <a
             href="https://twitter.com/intent/tweet?screen_name=anakagungcorp&ref_src=twsrc%5Etfw"
-            class="twitter-mention-button"
+            class="link-gradient"
             data-show-count="false"
         >
             hi to me.
@@ -180,9 +231,7 @@
     }
 
     .title {
-        font-family: "AlienLeague Bold", -apple-system, BlinkMacSystemFont,
-            "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
-            "Helvetica Neue", sans-serif;
+        font-family: "AlienLeague Bold", var(--default-text);
         font-size: 56px;
         text-align: center;
     }
@@ -203,9 +252,9 @@
         font-size: 10em;
         background: linear-gradient(
             90deg,
-            rgba(0, 0, 0, 0.4),
+            var(--shadow-black-4),
             transparent,
-            rgba(0, 0, 0, 0.4)
+            var(--shadow-black-4)
         );
         backdrop-filter: blur(45px);
         transform: scale(5);
@@ -231,18 +280,50 @@
         animation: waveHand 3s ease-in-out infinite alternate;
     }
 
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        margin-bottom: 8px;
+    }
+
+    .stack-link {
+        line-height: 1.3;
+        letter-spacing: 0.5px;
+        text-decoration: none;
+        padding-left: 4px;
+    }
+
+    .stack-link:link,
+    .stack-link:visited {
+        color: var(--primary-light);
+        border-left: 1px solid var(--grayish);
+        border-bottom: 1px solid var(--grayish);
+        transition-property: color, border;
+        transition-duration: 1200ms;
+        transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+    }
+
+    .stack-link:hover,
+    .stack-link:active {
+        color: var(--tangerine);
+        border-left: 1px solid var(--tangerine-light-alpha-8);
+        border-bottom: 1px solid var(--tangerine-light-alpha-8);
+    }
+
     .social-media__title {
         text-align: center;
     }
 
-    .twitter-mention-button {
+    .link-gradient {
         position: relative;
         color: var(--secondary);
         background: var(--primary);
         mix-blend-mode: lighten;
     }
 
-    .twitter-mention-button:before {
+    .link-gradient:before {
         content: "";
         background: linear-gradient(
             125deg,
@@ -257,6 +338,7 @@
         bottom: 0;
         right: 0;
         mix-blend-mode: darken;
+        pointer-events: none;
     }
 
     .social-media__lists {
