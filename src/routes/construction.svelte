95ca066d67e3d3981778b2ea3c009bbee5f9b3fc
Padunk <abraham.agel@gmail.com>
2020-12-02 15:00:02 +0700

    new route

---
+++ src/routes/construction.svelte
@@ -0,0 +1,44 @@
+<h2>Site is under contruction</h2>
+<div class="img-wrapper">
+    <img
+        src="/icons/undraw_under_construction_46pa.svg"
+        alt="under construction"
+    />
+</div>
+<p>
+    <small>SVG picture from
+        <a
+            href="https://undraw.co"
+            target="_blank"
+            rel="noopener noreferrer"
+        >undraw.co</a></small>
+</p>
+
+<style>
+    h2,
+    p {
+        text-align: center;
+    }
+
+    .img-wrapper {
+        display: flex;
+        justify-content: center;
+        height: calc(100vh - 120px);
+        width: 100vw;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    a:link,
+    a:visited {
+        color: var(--secondary);
+        transition: color 200ms ease-in-out;
+    }
+
+    a:hover,
+    a:active {
+        color: var(--tangerine);
+    }
+</style>
