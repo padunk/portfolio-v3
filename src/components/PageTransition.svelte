bb9d19a76367d490cf61661f870c9cde96b37e09
Padunk <abraham.agel@gmail.com>
2020-12-03 16:14:57 +0700

    transition wrapper

---
+++ src/components/PageTransition.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { cubicInOut } from "svelte/easing";
+    import { fade, fly } from "svelte/transition";
+
+    let duration = 250;
+    let delay = duration;
+
+    const transitionIn = () => ({
+        duration,
+        delay,
+        easing: cubicInOut,
+        css: (t) => `opacity: ${t}`,
+    });
+
+    const transitionOut = () => ({
+        duration,
+        delay: 0,
+        easing: cubicInOut,
+        css: (t) => `opacity: ${t}`,
+    });
+</script>
+
+<div in:fly|local="{{ x: 2000, duration: 2000 }}" out:fade|local>
+    <slot />
+</div>
