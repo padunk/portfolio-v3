349ef6350dedbb09ff3447a7047813b8e5c8a08c
Padunk <abraham.agel@gmail.com>
2020-12-08 15:17:56 +0700

    404 page

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,40 +1,69 @@
 <script lang="ts">
-	export let status: number;
-	export let error: Error;
+    import ErrorIcon from "../components/Icons/ErrorIcon.svelte";
 
-	const dev = process.env.NODE_ENV === 'development';
+    export let status: number;
+    export let error: Error;
+
+    const dev = process.env.NODE_ENV === "development";
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
-	<title>{status}</title>
+    <title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<div class="wrapper">
+    <h1>{status}</h1>
 
-<p>{error.message}</p>
+    <p>{error.message}</p>
 
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+    <ErrorIcon size="{200}" />
+
+    {#if dev && error.stack}
+        <pre>{error.stack}</pre>
+    {/if}
+    <p>
+        <small>SVG picture from
+            <a
+                href="https://undraw.co"
+                target="_blank"
+                rel="noopener noreferrer"
+            >undraw.co</a></small>
+    </p>
+</div>
+
+<style>
+    .wrapper {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: white;
+    }
+
+    h1 {
+        font-size: 2.8em;
+        font-weight: 700;
+    }
+
+    p {
+        margin: 1em auto;
+    }
+
+    a:link,
+    a:visited {
+        color: var(--secondary);
+        transition: color 200ms ease-in-out;
+    }
+
+    a:hover,
+    a:active {
+        color: var(--tangerine);
+    }
+
+    @media (min-width: 480px) {
+        h1 {
+            font-size: 4em;
+        }
+    }
+</style>
