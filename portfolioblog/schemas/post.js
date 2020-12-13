dd1f9d8b2d52302e408ad8ffe17aee26c43ea253
Padunk <abraham.agel@gmail.com>
2020-12-13 11:58:57 +0700

    adding date-fns

--- portfolioblog/schemas/post.js
+++ portfolioblog/schemas/post.js
@@ -1,3 +1,5 @@
+import { format, parseISO } from "date-fns";
+
 export default {
     name: "post",
     title: "Post",
@@ -96,7 +98,7 @@ export default {
             media: "mainImage",
         },
         prepare({ title = "No title", publishedAt, slug, media }) {
-            const dateSegment = format(publishedAt, "YYYY/MM");
+            const dateSegment = format(parseISO(publishedAt), "yyyy/MM/dd");
             const path = `/${dateSegment}/${slug.current}/`;
             return {
                 title,
