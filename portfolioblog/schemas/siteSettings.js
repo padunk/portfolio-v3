9a36305e08a7e9e2a8fdf172f2c8cacbe165333c
Padunk <abraham.agel@gmail.com>
2020-12-13 11:59:11 +0700

    formatted

--- portfolioblog/schemas/siteSettings.js
+++ portfolioblog/schemas/siteSettings.js
@@ -1,36 +1,37 @@
 export default {
-  name: 'siteSettings',
-  type: 'document',
-  title: 'Site Settings',
-  __experimental_actions: ['update', /* 'create', 'delete', */ 'publish'],
-  fields: [
-    {
-      name: 'title',
-      type: 'string',
-      title: 'Title'
-    },
-    {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-      description: 'Describe your blog for search engines and social media.'
-    },
-    {
-      name: 'keywords',
-      type: 'array',
-      title: 'Keywords',
-      description: 'Add keywords that describes your blog.',
-      of: [{type: 'string'}],
-      options: {
-        layout: 'tags'
-      }
-    },
-    {
-      name: 'author',
-      type: 'reference',
-      description: 'Publish an author and set a reference to them here.',
-      title: 'Author',
-      to: [{type: 'author'}]
-    }
-  ]
-}
+    name: "siteSettings",
+    type: "document",
+    title: "Site Settings",
+    __experimental_actions: ["update", /* 'create', 'delete', */ "publish"],
+    fields: [
+        {
+            name: "title",
+            title: "Title",
+            type: "string",
+        },
+        {
+            name: "description",
+            title: "Description",
+            type: "text",
+            description:
+                "Describe your blog for search engines and social media.",
+        },
+        {
+            name: "keywords",
+            title: "Keywords",
+            type: "array",
+            description: "Add keywords that describes your blog.",
+            of: [{ type: "string" }],
+            options: {
+                layout: "tags",
+            },
+        },
+        {
+            name: "author",
+            title: "Author",
+            type: "reference",
+            description: "Publish an author and set a reference to them here.",
+            to: [{ type: "author" }],
+        },
+    ],
+};
