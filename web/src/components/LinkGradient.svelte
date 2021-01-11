e0b3db5f601869c7c7ec21ed5ce50ed5963e9db6
Padunk <abraham.agel@gmail.com>
2021-01-11 12:38:27 +0700

    new link component

---
+++ web/src/components/LinkGradient.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let href;
+    export let text;
+</script>
+
+<a
+    href="{href}"
+    class="link-gradient"
+    target="_blank"
+    rel="noopener noreferrer"
+>
+    {text}
+</a>
+
+<style>
+    .link-gradient:link,
+    .link-gradient:visited {
+        color: var(--primary-light);
+        transition: all 200ms ease-in-out;
+        font-weight: 600;
+    }
+
+    .link-gradient:hover,
+    .link-gradient:active {
+        background: linear-gradient(
+            90deg,
+            var(--tertiary),
+            var(--secondary-bright)
+        );
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        -webkit-box-decoration-break: clone;
+        background-clip: text;
+        box-decoration-break: clone;
+        text-shadow: none;
+    }
+</style>
